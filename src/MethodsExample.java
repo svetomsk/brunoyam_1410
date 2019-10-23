42c3ea3edc76228f635e1e9c5e50888a82d74b61
123 <123>
2019-10-23 22:04:57 +0300

    Classes beginning

--- src/MethodsExample.java
+++ src/MethodsExample.java
@@ -1,14 +1,55 @@
+import java.util.Random;
+
 public class MethodsExample {
+
     public static void main(String[] args) {
-        int result = sumOfNumbers(6, 5);
+        int k = 6;
+        int result = sumOfNumbers(k, 5);
         System.out.println(result);
+        System.out.println(k);
+
+        int [] array = {1, 2, -23, -231};
+        System.out.println(array[0]);
+        int [] anotherArray = changeArray(array);
+        anotherArray[0] = 1234;
+        System.out.println(array[0]);
+        System.out.println(countNegative(array));
     }
 
     public static int sumOfNumbers(int a, int b) {
         // int a = 6;
         // int b = 5;
         int result = a + b;
+        a = 15;
         return result;
+    }
+
+    static int[] changeArray(int [] mas) {
+        mas[0] = 123;
+        return mas;
+    }
+
+    static int countNegative(int[] mas) {
+        int count = 0;
+        for(int e : mas) {
+            if(e < 0) {
+               count++;
+            }
+        }
+        return count;
+    }
+
+    static int countEvenElements(int [] mas) {
+        int sum = 0;
+        for (int i = 0; i < mas.length; i++) {
+            if(i % 2 == 0) {
+                sum += mas[i];
+            }
+//            if(mas[i] % 2 == 0) {
+//                sum += mas[i];
+//            }
+        }
+        return sum;
     }
 
     private void sum () {
---
+++ src/Point3D.java
@@ -0,0 +1,50 @@
+public class Point3D {
+    double x;
+    double y;
+    double z;
+
+    public static void printPoint(Point3D p) {
+        System.out.println(p.x + " " + p.y + " " + p.z);
+    }
+
+    public double distance() {
+        double dist = Math.sqrt(x*x + y*y + z*z);
+        return dist;
+    }
+}
+
+class PointCalculator {
+    public static void main(String[] args) {
+        Point3D.printPoint(new Point3D());
+
+        Point3D[] points = new Point3D[1];
+        points[0] = new Point3D();
+        System.out.println(points[0]);
+        // init, fill array
+        double totalDistance = 0;
+        for(Point3D p : points) {
+            totalDistance += p.distance();
+//            p.printPoint(p);
+            Point3D.printPoint(p);
+        }
+        System.out.println(totalDistance);
+    }
+}
+
+class BadPointCalculator {
+    static double distance(double x, double y, double z) {
+        return Math.sqrt(x*x + y*y + z*z);
+    }
+    public static void main(String[] args) {
+        double [] x = new double[1000];
+        double [] y = new double[1000];
+        double [] z = new double[1000];
+        // init, fill arrays
+        double totalDistance = 0;
+        for (int i = 0; i < x.length; i++) {
+            double dist = distance(x[i], y[i], z[i]);
+            totalDistance += dist;
+        }
+        System.out.println(totalDistance);
+    }
+}
